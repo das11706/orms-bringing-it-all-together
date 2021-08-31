6269fc218d87b0fa5ff3660ba131d48aa08cbdd0
Daniel Suarez <suarez.daniel101@gmail.com>
2021-08-31 17:49:03 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,4 +55,15 @@ class Dog
     new_dog
   end
 
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * 
+      FROM dogs
+      WHERE id = ?
+    SQL
+
+    row = DB[:conn].execute(sql, id)[0]
+    self.new(id: row[0], name: row[1], breed: row[2])
+  end
+
 end
